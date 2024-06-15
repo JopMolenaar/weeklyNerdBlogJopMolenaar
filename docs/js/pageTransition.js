6ded9af50fcc0b78272056499c4c382e0615ad2e
Jop <jopmolenaar@icloud.com>
2024-06-15 17:04:15 +0200

    added viewtransitions

--- docs/js/pageTransition.js
+++ docs/js/pageTransition.js
@@ -1,24 +1,41 @@
 const links = document.querySelectorAll("a");
-if (links) {
-    links.forEach((link) => {
-        link.onclick = (e) => {
-            const body = document.querySelector("body");
-            const parent = link.parentElement;
-            e.preventDefault();
-            setTimeout(function () {
-                if (body.classList.contains("fade-out")) {
-                    console.log("navigating");
-                    if (!e.srcElement.parentElement.href) {
-                        window.location = e.srcElement.href;
-                    } else {
-                        window.location = e.srcElement.parentElement.href;
-                    }
-                } else {
-                    console.log("whoops", e.srcElement.parentElement.href);
-                }
-            }, 500);
-            body.classList.add("fade-out");
-            parent.classList.add("grow");
-        };
-    });
+// if (links) {
+//     links.forEach((link) => {
+//         link.onclick = (e) => {
+//             const body = document.querySelector("body");
+//             const parent = link.parentElement;
+//             e.preventDefault();
+//             setTimeout(function () {
+//                 if (body.classList.contains("fade-out")) {
+//                     console.log("navigating");
+//                     if (!e.srcElement.parentElement.href) {
+//                         window.location = e.srcElement.href;
+//                     } else {
+//                         window.location = e.srcElement.parentElement.href;
+//                     }
+//                 } else {
+//                     console.log("whoops", e.srcElement.parentElement.href);
+//                 }
+//             }, 500);
+//             body.classList.add("fade-out");
+//             parent.classList.add("grow");
+//         };
+//     });
+// }
+
+links.forEach((link)=>[
+link.addEventListener((e)=>[
+    handleClick(e)
+])
+])
+
+function handleClick(e) {
+    // Fallback for browsers that don't support this API:
+    if (!document.startViewTransition) {
+        updateTheDOMSomehow();
+        return;
+    }
+
+    // With a View Transition:
+    document.startViewTransition(() => updateTheDOMSomehow());
 }
